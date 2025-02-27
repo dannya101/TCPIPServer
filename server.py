32b66a98cc2db9dc47def4ba388123deb58dc25f
Isabelle Bernal <71413422+isabellebernal@users.noreply.github.com>
2025-02-27 11:46:01 -0800

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,16 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 7070)
 
-print(f"starting up {server_address}" )
+print(f"starting up {server_address}")
 sock.bind(server_address)
 
 sock.listen(1)
+
+def encode_msg(message):
+    msg_converted = ""
+    for i in message:
+        msg_converted += chr((ord(i) + 1) % 256)
+    return msg_converted
 
 while True:
     print("waiting")
@@ -15,19 +21,19 @@ while True:
     try:
         print(f"connection from {client_address}")
         while True:
-            #no more than 256 characters for a message
+            # no more than 256 characters for a message
             data = connection.recv(256)
             print(f"received {data}")
             if data:
                 print("message received")
-                #msg = str(data)
+                # msg = str(data)
                 msg = data.decode('utf-8')
                 print(f"encoded message: {msg}")
-                #this is where we would need to convert message to the encoded message
-                
-
-                msg = bytes(msg, 'utf-8')
-                connection.sendall(msg)
+                # this is where we would need to convert message to the encoded message
+                encoded_message = encode_msg(msg)
+                print(f"encoded message: {encoded_message}")
+                # converts message to bytes and sends back
+                connection.sendall(encoded_message.encode('utf-8'))
             else:
                 print(f"no more data from {client_address}")
                 break
