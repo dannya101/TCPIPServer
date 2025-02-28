ace8d28a8b2bbbe4d4a0c0049d647c94299fd1a3
abed-andrea <andrea.abed03@gmail.com>
2025-02-28 14:27:20 -0800

    Update add prince's changes

--- client1.py
+++ client1.py
@@ -44,35 +44,41 @@ if len(message) > 256:
     print("Error: Message exceeds 256 characters. Please enter a shorter message.")
     sys.exit(1)
 
-# create a socket / endpoint for communication with the server
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect to the server's address and port
-server_address = ('localhost', 7070)
-print('connecting to %s port %s' % server_address)
+def start_client():
+    # create a socket / endpoint for communication with the server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    client_socket.connect(server_address)
-except ConnectionRefusedError:
-    # throw an error if client tries to connect without server running
-    print("Error: Unable to connect to the server. Make sure the server is running.")
-    sys.exit(1)
+    # connect to the server's address and port
+    server_address = ('localhost', 7070)
+    print('connecting to %s port %s' % server_address)
+
+    try:
+        client_socket.connect(server_address)
+    except ConnectionRefusedError:
+        # throw an error if client tries to connect without server running
+        print("Error: Unable to connect to the server. Make sure the server is running.")
+        sys.exit(1)
 
 
-try:
-    # send the message to the server
-    print("sending ", message)
-    client_socket.sendall(bytes(message, 'utf-8'))   # .sendall ensures entire message is sent in one go 
+    try:
+        # send the message to the server
+        print("sending ", message)
+        client_socket.sendall(bytes(message, 'utf-8'))   # .sendall ensures entire message is sent in one go 
 
-    # look for a response back
-    data = client_socket.recv(256)
-    print("received ", data)
-    # convert received response from bytes to string in order to disply 
-    converted = data.decode('utf-8')
-    print("converted message:", converted)
+        # look for a response back
+        data = client_socket.recv(256)
+        print("received ", data)
+        # convert received response from bytes to string in order to disply 
+        converted = data.decode('utf-8')
+        print("converted message:", converted)
 
-finally:
-    print('closing client')
-    # close the communication socket on the client side
-    client_socket.close()
+    finally:
+        print('closing client')
+        # close the communication socket on the client side
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    start_client()
 
--- server.py
+++ server.py
@@ -62,41 +62,45 @@ def encode_msg(message):
         msg_converted += chr(ord(i) + 1)
     return msg_converted
 
-try:
-    # keep the server running continously to accept multiple client connections
-    while True:
-        # server waits until a client tries to connect
-        print("waiting")
-        # when a client connects, server calls accept which creates a new socket/address for actual communication
-        connection, client_address = server_socket.accept()
-        print("connection from ", client_address)
+def start_server():
+    try:
+        # keep the server running continously to accept multiple client connections
+        while True:
+            # server waits until a client tries to connect
+            print("waiting")
+            # when a client connects, server calls accept which creates a new socket/address for actual communication
+            connection, client_address = server_socket.accept()
+            print("connection from ", client_address)
 
-        try:
-            # receive message from the client (already restricted to less than 256 characters from client side)
-            data = connection.recv(256)
-            print("received ", data)
+            try:
+                # receive message from the client (already restricted to less than 256 characters from client side)
+                data = connection.recv(256)
+                print("received ", data)
 
-            # if data exists, server processes it
-            if data:
-                # convert message from bytes into string
-                msg = data.decode('utf-8')
-                print("encoding message...")
-                # encode the message ascii characters
-                encoded_message = encode_msg(msg)
-                print("sending ", encoded_message)
-                # converts message into bytes again and sends back
-                connection.sendall(encoded_message.encode('utf-8'))
-            else:
-                print("No more data from ", client_address)
-                break
+                # if data exists, server processes it
+                if data:
+                    # convert message from bytes into string
+                    msg = data.decode('utf-8')
+                    print("encoding message...")
+                    # encode the message ascii characters
+                    encoded_message = encode_msg(msg)
+                    print("sending ", encoded_message)
+                    # converts message into bytes again and sends back
+                    connection.sendall(encoded_message.encode('utf-8'))
+                else:
+                    print("No more data from ", client_address)
+                    break
 
-        finally:
-            # close the communication socket on the server side
-            connection.close()
+            finally:
+                # close the communication socket on the server side
+                connection.close()
 
-except KeyboardInterrupt:
-    # finally close the listening socket on the server side
-    print("\nClosing server listening socket...")
-    server_socket.close()
+    except KeyboardInterrupt:
+        # finally close the listening socket on the server side
+        print("\nClosing server listening socket...")
+        server_socket.close()
+
+if __name__ == '__main__':
+    start_server()
 
 
