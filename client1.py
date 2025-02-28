3aa80c8e099ec1f0450e614d3f914e11591fbe5d
abed-andrea <andrea.abed03@gmail.com>
2025-02-27 23:49:58 -0800

    Updated client1.py and server.py - added comments, improved error handling

--- client1.py
+++ client1.py
@@ -1,27 +1,78 @@
+# import socket
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# server_address = ('localhost', 7070)
+# print('connecting to %s port %s' % server_address)
+# sock.connect(server_address)
+
+# try:
+#     #message that is being sent to server
+#     message = 'Hello World'
+#     print(f'sending {message}')
+#     sock.sendall(bytes(message, 'utf-8'))
+
+#     # look for a response back
+#     amount_received = 0
+#     amount_expected = len(message)
+
+#     while amount_received < amount_expected:
+#         #no more than 256 characters
+#         data = sock.recv(256)
+#         amount_received += len(data)
+#         print(f'received {data}')
+
+# finally:
+#     print('closing client')
+#     sock.close()
+
+
+print("In client file")
 import socket
+import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# check if the user provided a message argument
+if len(sys.argv) < 2:
+    print("Error: Please enter a message.")
+    sys.exit(1)
 
+# get the message from the command-line argument
+message = sys.argv[1]
+
+# check if the message length is within the 256-character limit
+if len(message) > 256:
+    print("Error: Message exceeds 256 characters. Please enter a shorter message.")
+    sys.exit(1)
+
+# create a socket / endpoint for communication with the server
+client_socket = socket.socket()
+
+# connect to the server's address and port
 server_address = ('localhost', 7070)
 print('connecting to %s port %s' % server_address)
-sock.connect(server_address)
 
 try:
-    #message that is being sent to server
-    message = 'Hello World'
-    print(f'sending {message}')
-    sock.sendall(bytes(message, 'utf-8'))
+    client_socket.connect(server_address)
+except ConnectionRefusedError:
+    # throw an error if client tries to connect without server running
+    print("Error: Unable to connect to the server. Make sure the server is running.")
+    sys.exit(1)
+
+
+try:
+    # send the message to the server
+    print("sending ", message)
+    client_socket.sendall(bytes(message, 'utf-8'))   # .sendall ensures entire message is sent in one go 
 
     # look for a response back
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        #no more than 256 characters
-        data = sock.recv(256)
-        amount_received += len(data)
-        print(f'received {data}')
+    data = client_socket.recv(256)
+    print("received ", data)
+    # convert received response from bytes to string in order to disply 
+    converted = data.decode('utf-8')
+    print("converted message:", converted)
 
 finally:
     print('closing client')
-    sock.close()
+    # close the communication socket on the client side
+    client_socket.close()
+
--- server.py
+++ server.py
@@ -1,42 +1,102 @@
+# import socket
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_address = ('localhost', 7070)
+
+# print(f"starting up {server_address}")
+# sock.bind(server_address)
+
+# sock.listen(1)
+
+# def encode_msg(message):
+#     msg_converted = ""
+#     for i in message:
+#         msg_converted += chr((ord(i) + 1) % 256)
+#     return msg_converted
+
+# while True:
+#     print("waiting")
+#     connection, client_address = sock.accept()
+
+#     try:
+#         print(f"connection from {client_address}")
+#         while True:
+#             # no more than 256 characters for a message
+#             data = connection.recv(256)
+#             print(f"received {data}")
+#             if data:
+#                 print("message received")
+#                 # msg = str(data)
+#                 msg = data.decode('utf-8')
+#                 print(f"encoded message: {msg}")
+#                 # this is where we would need to convert message to the encoded message
+#                 encoded_message = encode_msg(msg)
+#                 print(f"encoded message: {encoded_message}")
+#                 # converts message to bytes and sends back
+#                 connection.sendall(encoded_message.encode('utf-8'))
+#             else:
+#                 print(f"no more data from {client_address}")
+#                 break
+
+#     finally:
+#         connection.close()
+
+
+print("In server file")
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 7070)
+# server creates a socket / ommunication endpoint that waits for connections -- specifies ipv4 addressing and tcp
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(f"starting up {server_address}")
-sock.bind(server_address)
+# server uses bind to assign the socket to specific ip address and port number
+server_socket_addr = ('localhost', 7070)
+server_socket.bind(server_socket_addr)
 
-sock.listen(1)
+# server tells the system it is ready to accept connections
+server_socket.listen(1)
 
+# define function for encoding message -- each letter in message converted to next ascii character
 def encode_msg(message):
     msg_converted = ""
     for i in message:
-        msg_converted += chr((ord(i) + 1) % 256)
+        msg_converted += chr(ord(i) + 1)
     return msg_converted
 
-while True:
-    print("waiting")
-    connection, client_address = sock.accept()
+try:
+    # keep the server running continously to accept multiple client connections
+    while True:
+        # server waits until a client tries to connect
+        print("waiting")
+        # when a client connects, server calls accept which creates a new socket/address for actual communication
+        connection, client_address = server_socket.accept()
+        print("connection from ", client_address)
 
-    try:
-        print(f"connection from {client_address}")
-        while True:
-            # no more than 256 characters for a message
+        try:
+            # receive message from the client (already restricted to less than 256 characters from client side)
             data = connection.recv(256)
-            print(f"received {data}")
+            print("received ", data)
+
+            # if data exists, server processes it
             if data:
-                print("message received")
-                # msg = str(data)
+                # convert message from bytes into string
                 msg = data.decode('utf-8')
-                print(f"encoded message: {msg}")
-                # this is where we would need to convert message to the encoded message
+                print("encoding message...")
+                # encode the message ascii characters
                 encoded_message = encode_msg(msg)
-                print(f"encoded message: {encoded_message}")
-                # converts message to bytes and sends back
+                print("sending ", encoded_message)
+                # converts message into bytes again and sends back
                 connection.sendall(encoded_message.encode('utf-8'))
             else:
-                print(f"no more data from {client_address}")
+                print("No more data from ", client_address)
                 break
 
-    finally:
-        connection.close()
+        finally:
+            # close the communication socket on the server side
+            connection.close()
+
+except KeyboardInterrupt:
+    # finally close the listening socket on the server side
+    print("\nClosing server listening socket...")
+    server_socket.close()
+
+
